d8f1f0c7571552dc34ce0c4d4768a25529bdf170
Yashasvi252 <102022336+Yashasvi252@users.noreply.github.com>
2023-07-24 16:14:42 +0530

    uploaded
    uploaded successfully

---
+++ sync task3/js/data.js
@@ -0,0 +1,45 @@
+let songs =[
+    {
+        name: 'Bones',
+        path: 'songs/music1.mp3',
+        artist: 'Imagine Dragons',
+        cover: 'images/music1.jpg'
+    },
+    {
+        name: 'People',
+        path: 'songs/music2.mp3',
+        artist: 'Libianca Fonjis',
+        cover: 'images/music2.jpg'
+    },
+    {
+        name: 'See You Again',
+        path: 'songs/music3.mp3',
+        artist: 'The Chainsmokers, Illenium, Carlie Hanson',
+        cover: 'images/music3.jpg'
+    },
+    {
+        name: 'Shape Of You',
+        path: 'songs/music4.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'images/music4.jpg'
+    },
+    {
+        name: 'Closer',
+        path: 'songs/music5.mp3',
+        artist: 'The Chainsmokers,  Halsey',
+        cover: 'images/music5.jpg'
+    },
+    {
+        name: 'Locked Away',
+        path: 'songs/music6.mp3',
+        artist: 'Adam Levine',
+        cover: 'images/music6.jpeg'
+    },
+    {
+        name: 'Deandlions',
+        path: 'songs/music7.mp3',
+        artist: 'Adam Levine',
+        cover: 'images/music7.jpg'
+    }
+    
+]
